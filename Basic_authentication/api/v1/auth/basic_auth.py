395337e295d416a057cd4d8e6c7a2e25f1ad3c22
Ineza-Agape <i.agape@alustudent.com>
2025-10-01 12:38:42 +0200

    added models/

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,7 @@ Basic auth class
 from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
-from ...models.user import User
+from models.user import User
 
 
 class BasicAuth(Auth):
